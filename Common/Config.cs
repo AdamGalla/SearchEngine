2c9e94221aa32fe0d1316235598950fea8219855
AdamGalla <ado.galla@gmail.com>
2023-03-02 11:13:31 +0100

    Config change

--- Common/Config.cs
+++ Common/Config.cs
@@ -4,8 +4,8 @@ namespace Common
 {
     public static class Config
     {
-        public static string DatabasePath { get; } = @"C:\Program Files (x86)\SQLite\SearchEngine.db";
-        public static string DataSourcePath { get; } = "C:\\Users\\adoga\\OneDrive\\Dokumenty\\DataSets\\enron_mail_20150507\\maildir";
+        public static string DatabasePath { get; } = "C:\\DLS\\SearchEngine.db";
+        public static string DataSourcePath { get; } = "C:";
         public static int NumberOfFoldersToIndex { get; } = 0; // Use 0 or less for indexing all folders
     }
 }
