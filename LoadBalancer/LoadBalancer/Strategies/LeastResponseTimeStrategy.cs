a64d1f10be4150e0be2a20374c8634cf785e9d08
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-03-09 12:53:17 +0100

    Update LeastResponseTimeStrategy.cs
    Made it default to the first service if the previous method didnt work

--- LoadBalancer/LoadBalancer/Strategies/LeastResponseTimeStrategy.cs
+++ LoadBalancer/LoadBalancer/Strategies/LeastResponseTimeStrategy.cs
@@ -6,6 +6,6 @@ public class LeastResponseTimeStrategy : ILoadBalancerStrategy
 {
     public Service NextService(List<Service> services)
     {
-        return services.MinBy(service => service.AverageResponseTime);
+        return services.MinBy(service => service.AverageResponseTime) ?? services.First();
     }
 }
