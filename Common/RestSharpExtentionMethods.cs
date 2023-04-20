8adacee25586c5be0a53ee9aacda80e783f97178
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-04-20 13:45:24 +0200

    Create RestSharpExtentionMethods.cs
    methods to transfer tracing data over rest request
    rest request have to be changed to the new extension method

---
+++ Common/RestSharpExtentionMethods.cs
@@ -0,0 +1,24 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using RestSharp;
+using System.Diagnostics;
+
+namespace Common;
+public static class RestSharpExtentionMethods
+{
+    private static readonly TextMapPropagator Propagator = new TraceContextPropagator();
+
+    public static async Task<RestResponse<T>> ExecuteWithTracingAsync<T>(this IRestClient client, RestRequest request) //where T : new()
+    {
+        using var activity = Monitoring.ActivitySource.StartActivity(ActivityKind.Client);
+        var activityContext = activity?.Context ?? Activity.Current?.Context ?? default;
+
+        var propagationContext = new PropagationContext(activityContext, Baggage.Current);
+        Propagators.DefaultTextMapPropagator.Inject(propagationContext, request, (restRequest, key, value) =>
+        {
+            restRequest.AddHeader(key, value);
+        });
+
+        return await client.ExecuteAsync<T>(request);
+    }
+}
