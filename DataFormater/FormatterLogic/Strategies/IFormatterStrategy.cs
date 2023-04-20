9b3f1514ac2b282e24c019df9e987b26112f113c
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-04-20 10:56:34 +0200

    Update IFormatterStrategy.cs

--- DataFormater/FormatterLogic/Strategies/IFormatterStrategy.cs
+++ DataFormater/FormatterLogic/Strategies/IFormatterStrategy.cs
@@ -1,5 +1,8 @@
-﻿namespace DataFormatter.FormatterLogic.Strategies;
+﻿using DataFormatter.FormatterLogic.Model;
+
+namespace DataFormatter.FormatterLogic.Strategies;
 
 public interface IFormatterStrategy
 {
+    public string FormatText(FileData data);
 }
