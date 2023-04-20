6fca4805ed8c2367367afbb9079eeb316da0db9d
AdamGalla <ado.galla@gmail.com>
2023-04-20 11:16:02 +0200

    Format to excel strategy

--- DataFormater/FormatterLogic/Model/FileData.cs
+++ DataFormater/FormatterLogic/Model/FileData.cs
@@ -2,5 +2,9 @@
 
 public record FileData
 {
-
+    public string SearchWord { get; set; }
+    public List<KeyValuePair<int, int>>? DocIds { get; set; }
+    public List<int>? Top10 { get; set; }
+    public List<string>? Top10Details { get; set; }
+    public TimeSpan Used { get; set; }
 }
