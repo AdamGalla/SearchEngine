4c470a67e75a470abfe59b59d741b918562ba3e2
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-04-20 13:44:51 +0200

    Init tracing helper methods

---
+++ Common/Monitoring.cs
@@ -0,0 +1,34 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Common;
+public static class Monitoring
+{
+    public static readonly ActivitySource ActivitySource = new("SearchAPI_" + Assembly.GetEntryAssembly()?.GetName().Name, "1.0.0");
+    private static TracerProvider _tracerProvider;
+
+    static Monitoring()
+    {
+        // Configure tracing
+        var serviceName = Assembly.GetExecutingAssembly().GetName().Name;
+        var version = "1.0.0";
+
+        _tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddZipkinExporter()
+            .AddConsoleExporter()
+            .AddSource(ActivitySource.Name)
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: version))
+            .Build()!;
+
+        // Configure logging
+        /*Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .Enrich.WithSpan()
+            .WriteTo.Seq("http://localhost:5341")
+            .WriteTo.Console()
+            .CreateLogger();*/
+    }
+}
