4d12de923f6d5c7a16e25a1ed322fcc82e166d97
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-04-20 11:07:15 +0200

    Update FileData.cs

--- DataFormater/FormatterLogic/Model/FileData.cs
+++ DataFormater/FormatterLogic/Model/FileData.cs
@@ -2,5 +2,9 @@
 
 public record FileData
 {
-
+    public string SearchWord { get; set; }
+    public List<KeyValuePair<int, int>>? DocIds { get; set; } // the occurances of the search word
+    public List<int>? Top10 { get; set; }
+    public List<string>? Top10Details { get; set; } // the file paths
+    public TimeSpan Used { get; set; } // the time it took to find the results
 }
