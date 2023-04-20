ba5c2f7f5c6e05273b7242b4aab67c866888c67c
AdamGalla <ado.galla@gmail.com>
2023-04-20 10:13:08 +0200

    Init DataFormatter service

---
+++ DataFormater/Controllers/FormatterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DataFormater.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormatterController : ControllerBase
+    {
+        // GET: api/<FormatterController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FormatterController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FormatterController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FormatterController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FormatterController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
