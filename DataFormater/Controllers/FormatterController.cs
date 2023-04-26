121bd1c92b2d9ee49d26ef9a05fe7b6f07be1686
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-04-26 16:36:18 +0200

    Update FormatterController.cs
    Added context from recieved http request

--- DataFormater/Controllers/FormatterController.cs
+++ DataFormater/Controllers/FormatterController.cs
@@ -2,7 +2,9 @@
 using DataFormatter.FormatterLogic.Model;
 using DataFormatter.StrategyFactory;
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Context.Propagation;
 using System;
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
 
 namespace DataFormater.Controllers;
@@ -12,6 +14,8 @@ namespace DataFormater.Controllers;
 public class FormatterController : ControllerBase
 {
     private IStrategyFactory _strategyFactory;
+    private static readonly TextMapPropagator Propagator = new TraceContextPropagator();
+
     public FormatterController(IStrategyFactory strategyFactory)
     {
         _strategyFactory = strategyFactory;
@@ -22,8 +26,19 @@ public class FormatterController : ControllerBase
     public async Task<ActionResult<string>> GetAsync([FromBody] FileData data, string strategy)
     {
         //using var activity = Monitoring.ActivitySource.StartActivity(); handled by middleware
-        using var activity = Monitoring.ActivitySource.StartActivity();
+        //using var activity = Monitoring.ActivitySource.StartActivity();
         //Console.WriteLine(Monitoring.ActivitySource.HasListeners());
+        var parentContext = Propagator.Extract(default, Request.Headers, (headers, name) =>
+        {
+            if (headers.TryGetValue(name, out var value))
+            {
+                return new[] { value.ToString() };
+            }
+
+            return Enumerable.Empty<string>();
+        });
+
+        using var activity = Monitoring.ActivitySource.StartActivity("Received HTTP request", ActivityKind.Consumer, parentContext.ActivityContext);
 
         if (data is null)
         {
@@ -36,8 +51,6 @@ public class FormatterController : ControllerBase
         }
 
         string result = await _strategyFactory.GetStrategyType(strategyType).FormatTextAsync(data);
-
-        await Task.Delay(1000);
 
         return Ok(result);
     }
