a9f3a4cd226b8e568dc3a638fa35deb9e227e8d3
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-05-04 12:17:36 +0200

    Benchamrks init

---
+++ TestDataFormatter/Benchmark.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FeatureHubSDK;
+
+namespace TestDataFormatter;
+
+public class BenchmarkMain
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [Benchmark]
+    public IEnumerable<IClientContext> TestFeatureHubClientBuilder()
+    {
+        for (int i = 0; i < 3000; i++)
+        {
+            var featureHubCfg = new EdgeFeatureHubConfig("http://localhost:8085", "3996fba9-d379-4fa2-bbd4-0d027cf0c694/WZJlb0y374aZelOnCLtKtw76OFEErv38GRLUAnmO");
+            yield return featureHubCfg.NewContext().Build().GetAwaiter().GetResult();
+        }
+    }
+}
